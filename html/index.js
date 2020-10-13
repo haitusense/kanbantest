92f7477d05a7d93a3a740bf03737c3e89f8addac
合肥海图微电子有限公司 <49272093+haitusense@users.noreply.github.com>
2020-10-13 16:55:00 +0900

    Add files via upload

---
+++ html/index.js
@@ -0,0 +1,40 @@
+  const asyncOctokit = async () =>{ 
+    let hoge = await import('https://cdn.skypack.dev/@octokit/rest');
+    return new hoge.Octokit();
+  }
+  
+  const asyncGraphql = async () =>{
+    let dst = await import('https://cdn.skypack.dev/@octokit/graphql');
+    return dst;
+  }
+
+(async()=>{
+
+  window.babelConfig = {};
+
+  if(babelflag){
+    requirejs.config({
+      paths: {
+        'jsx': 'App/Logic/react-require.min',
+        'babel': 'https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.26.0/babel.min',
+        'react': 'https://unpkg.com/react@16/umd/react.development',
+        'react-dom': 'https://unpkg.com/react-dom@16/umd/react-dom.development',
+        'material-ui': 'https://unpkg.com/@material-ui/core@latest/umd/material-ui.production.min',
+        'immer': 'https://unpkg.com/immer@6.0.3/dist/immer.umd.production.min',
+      },
+    });
+  }else{
+    requirejs.config({
+      paths: {
+        'jsx': 'App/Logic/react-require.min',
+        'babel': 'https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.26.0/babel.min',
+        'react': 'https://unpkg.com/react@16/umd/react.production.min',
+        'react-dom': 'https://unpkg.com/react-dom@16/umd/react-dom.production.min',
+        'material-ui': 'https://unpkg.com/@material-ui/core@latest/umd/material-ui.production.min',
+        'immer': 'https://unpkg.com/immer@6.0.3/dist/immer.umd.production.min',
+      },
+    });     
+  }
+
+  require(['jsx!App/App']);
+})();
