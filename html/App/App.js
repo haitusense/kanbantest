54cfb8afdb69d9cfb852b8c9265cbd7a360d2770
合肥海图微电子有限公司 <49272093+haitusense@users.noreply.github.com>
2020-10-13 17:16:05 +0900

    Add files via upload

--- html/App/App.js
+++ html/App/App.js
@@ -1,1 +1,53 @@
+'use strict';
 
+define(
+  [
+    'react', 
+    'react-dom',
+    'jsx!App/Provider',
+    'jsx!App/Components/AppBar',
+    'jsx!App/Components/Common',
+    'material-ui',
+  ],
+  (
+    React,
+    ReactDOM, 
+    Provider,
+    AppBar,
+    { Selector, lazy, Copyright, Loading, ErrSnackbar },
+    { Box, CssBaseline },
+  )=>{
+
+  /* Regions */
+  const regionManeger = {
+    Default : (<div>under construction</div>),
+  };
+
+  /* EventListener */
+  const eventTarget = {
+    screenChanged : (dispatch)=> window.addEventListener("orientationchange", ()=>{
+      setTimeout(()=>{
+        const width = window.parent.screen.width
+        const height = window.parent.screen.height
+        dispatch({ type: ActionType.SCREEN, value:{width:width,height:height}});
+      }, 100);
+    }),
+  }
+  
+  const App = () => (
+    <Provider eventTarget={eventTarget}>
+      <AppBar title="Test" regions={regionManeger}>
+        <Loading/>
+        <ErrSnackbar/>
+        <Box mt={10} width="auto" minHeight={480}>
+          <Selector>{regionManeger}</Selector>
+        </Box>
+        <Box mt={3}>
+          <Copyright name={globalVariables.organization} href={globalVariables.href}/>
+        </Box>
+      </AppBar>
+    </Provider>
+  );
+
+  ReactDOM.render(<App />, document.getElementById('root'));
+});
